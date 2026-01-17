05533b0a68f99a98089b7cc997315edd1994e6ed
Vaishnavi-Mangati <vaishnavimangati@gmail.com>
2026-01-17 13:27:35 +0530

    Seperate result for different body types

---
+++ src/data/resultData.js
@@ -0,0 +1,14 @@
+export const resultData = {
+    Vata: {
+        title: "Vata Body Type",
+        description: "Energitic, free moving body"
+    },
+    Pitta: {
+        title: "Pitta Body Type",
+        description: "Bold, fierce"
+    },
+    Kapha: {
+        title: "Kapha Body Type",
+        description: "Calm, earthly"
+    }
+}
